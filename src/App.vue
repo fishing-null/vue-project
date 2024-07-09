a5c4a3c6570753e5908cc73be7321227e1b06a5b
MatouSakura <1134979484@qq.com>
2024-07-09 22:51:19 +0800

    listDemo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+<IfDemo />
+<HelloWorld />
+</template>
+
+<script setup>
+import IfDemo from "./components/ifDemo.vue"
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+
---
+++ src/components/ifDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-if="flag">flag标签为true且则显示，否则不显示</div>
+    <div v-else-if="type === 'C'">flag为flase且标签为D则显示</div>
+    <div v-else-if="type === 'D'">flag为flase且标签为D则显示</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                flag:false,
+                type:"D"
+            }
+        }
+    }
+</script>
