baf5ba2f2de6ef59d0aee414f50bc9c6445a2c35
MatouSakura <1134979484@qq.com>
2024-07-14 19:01:10 +0800

    a

--- src/components/FormBindingDemo.vue
+++ src/components/FormBindingDemo.vue
@@ -1,16 +1,20 @@
 <template>
-<p>你要打印的消息是: {{message}} </p>
-<input v-model="message" placeholder="在这里编辑"> </input>
-</template>
+    <div>
+        <p>你要打印的消息是: {{message}} </p>
+        <textarea v-model="message"></textarea>
+    </div>
 
 
-<script>
-export default{
-    data(){
-        return{
-            message:"",
+    <input v-model="message" placeholder="在这里编辑"> </input>
+    </template>
+    
+    <script>
+    export default{
+        data(){
+            return{
+                message:"",
+            }
         }
     }
-}
-
-</script>
+    
+    </script>
